60f10fafaa9d0327e60b30e031e1451edab29fe0
pekaem <pekaem61@gmail.com>
2021-07-27 23:00:05 +0100

    Heroku Deployment

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 require("./api-routes/reminder-routes.js")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
