990b2cfd6db7b9a517ef0c3f731ce22d16c9a262
pekaem <pekaem61@gmail.com>
2021-07-27 15:28:37 +0100

    Heroku Deploy

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,8 +1,9 @@
 import axios from "axios";
-
+// "https://mg-reminders.herokuapp.com/api"
+// "http://localhost:3000/api"
 export default axios.create({
   withCredentials: true,
-  baseURL: `https://mg-reminders.herokuapp.com/api`,
+  baseURL: "https://mg-reminders.herokuapp.com/api",
   headers: {
     "Content-type": "application/json"
   }
