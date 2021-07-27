187dc3c9001e8aacc8c1107479570b58168227aa
pekaem <pekaem61@gmail.com>
2021-07-27 17:11:53 +0100

    Heroku Deploy

--- models/index.js
+++ models/index.js
@@ -1,20 +1,26 @@
 const dbConfig = require("../db.config");
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  operatorsAliases: 0,
-  ssl: {
-    rejectUnauthorized: false
-  },
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
-  }
-});
+
+let sequelize = null;
+if(process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {dialect: "postgres", dialectOptions: { ssl: { rejectUnauthorized: false }}});
+} else {
+  sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    operatorsAliases: 0,
+    ssl: {
+      rejectUnauthorized: false
+    },
+    pool: {
+      max: dbConfig.pool.max,
+      min: dbConfig.pool.min,
+      acquire: dbConfig.pool.acquire,
+      idle: dbConfig.pool.idle
+    }
+  }); 
+}
 
 const db = {};
 
